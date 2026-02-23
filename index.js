48735751684336b40f897aab9a0b5d5a722256a3
ssyn02 <nicolasgerbaudo1@gmail.com>
2026-02-23 20:31:05 -0300

    all discrete distributions, js type conversion cleanup

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ document.addEventListener("DOMContentLoaded", function () {
             })
         });
 
+        document.getElementById("warning").textContent = "";
+
         const selected = this.value;
         if (selected) {
             document.getElementById(selected).style.display = "block";
@@ -59,7 +61,7 @@ document.addEventListener("DOMContentLoaded", function () {
         if (isNaN(n) || isNaN(p)) return;
 
         if (n < 0) {
-        document.getElementById("warning").textContent = "n must be ≥ 0";
+        document.getElementById("warning").textContent = "n cannot be negative";
         document.getElementById("binomial-mean").textContent = 0;
         document.getElementById("binomial-variance").textContent = 0;
         document.getElementById("binomial-sd").textContent = 0;
@@ -67,7 +69,7 @@ document.addEventListener("DOMContentLoaded", function () {
         }
 
         if (x < 0 || (x > n && n != 0)) {
-        document.getElementById("warning").textContent = "x must be ≥ 0 and ≤ n";
+        document.getElementById("warning").textContent = "x must be between 0 and n";
         document.getElementById("binomial-probability").textContent = 0;
         document.getElementById("binomial-cumulative").textContent = 0;
         return;
@@ -83,24 +85,23 @@ document.addEventListener("DOMContentLoaded", function () {
         
         document.getElementById("warning").textContent = "";
 
-        const mean = Number((n*p).toFixed(4));
-        const variance = Number((n*p*(1-p)).toFixed(4));
-        const sd = Number((Math.sqrt(variance)).toFixed(4));
+        const mean = (n*p);
+        const variance = (n*p*(1-p));
+        const sd = (Math.sqrt(variance));
         const aux = binomialProbability(n, p);
-        const probability = Number(aux(x)).toFixed(5);
-        const cumulative = Number(discreteCumulative(aux, x)).toFixed(5);
+        const probability = aux(x);
+        const cumulative = discreteCumulative(aux, x);
 
-        document.getElementById("binomial-mean").textContent = mean;
-        document.getElementById("binomial-variance").textContent = variance;
-        document.getElementById("binomial-sd").textContent = sd;
-        document.getElementById("binomial-probability").textContent = probability;
-        document.getElementById("binomial-cumulative").textContent = cumulative;
+        document.getElementById("binomial-mean").textContent = mean.toFixed(4);
+        document.getElementById("binomial-variance").textContent = variance.toFixed(4);
+        document.getElementById("binomial-sd").textContent = sd.toFixed(4);
+        document.getElementById("binomial-probability").textContent = probability.toFixed(5);
+        document.getElementById("binomial-cumulative").textContent = cumulative.toFixed(5);
     }
     binomial_n.addEventListener("input", updateBinomial);
     binomial_p.addEventListener("input", updateBinomial);
     binomial_x.addEventListener("input", updateBinomial);
     // #endregion
-
     // #region Poisson
     function poissonProbability(l) {
         return function(x){
@@ -120,7 +121,7 @@ document.addEventListener("DOMContentLoaded", function () {
         const x = Number(poisson_x.value);
 
         if (l < 0) {
-        document.getElementById("warning").textContent = "λ must be ≥ 0";
+        document.getElementById("warning").textContent = "λ cannot be negative";
         document.getElementById("binomial-mean").textContent = 0;
         document.getElementById("binomial-variance").textContent = 0;
         document.getElementById("binomial-sd").textContent = 0;
@@ -128,7 +129,7 @@ document.addEventListener("DOMContentLoaded", function () {
         }
 
         if (x < 0) {
-        document.getElementById("warning").textContent = "x must be ≥ 0";
+        document.getElementById("warning").textContent = "x cannot be negative";
         document.getElementById("binomial-probability").textContent = 0;
         document.getElementById("binomial-cumulative").textContent = 0;
         return;
@@ -139,23 +140,191 @@ document.addEventListener("DOMContentLoaded", function () {
             return;
         }
 
-        const mean = Number(l.toFixed(4));
-        const variance = Number(l.toFixed(4));
-        const sd = Number((Math.sqrt(variance)).toFixed(4));
+        const mean = l;
+        const variance = l;
+        const sd = Math.sqrt(variance);
         const aux = poissonProbability(l);
-        const probability = Number(aux(x)).toFixed(5);
-        const cumulative = Number(discreteCumulative(aux, x)).toFixed(5);
+        const probability = aux(x);
+        const cumulative = discreteCumulative(aux, x);
 
-        document.getElementById("poisson-mean").textContent = mean;
-        document.getElementById("poisson-variance").textContent = variance;
-        document.getElementById("poisson-sd").textContent = sd;
-        document.getElementById("poisson-probability").textContent = probability;
-        document.getElementById("poisson-cumulative").textContent = cumulative;
+        document.getElementById("poisson-mean").textContent = mean.toFixed(4);
+        document.getElementById("poisson-variance").textContent = variance.toFixed(4);
+        document.getElementById("poisson-sd").textContent = sd.toFixed(4);
+        document.getElementById("poisson-probability").textContent = probability.toFixed(5);
+        document.getElementById("poisson-cumulative").textContent = cumulative.toFixed(5);
 
         document.getElementById("warning").textContent = "";
     }
     poisson_l.addEventListener("input", updatePoisson);
     poisson_x.addEventListener("input", updatePoisson);
+    // #endregion
+    // #region Degenerate
+    const degenerate_c = document.getElementById("degenerate-c");
+    const degenerate_x = document.getElementById("degenerate-x");
+
+    function updateDegenerate() {
+        const c = Number(degenerate_c.value);
+        const x = Number(degenerate_x.value);
+
+        if (x < 0) {
+        document.getElementById("warning").textContent = "x must be ≥ 0";
+        document.getElementById("degenerate-probability").textContent = 0;
+        return;
+        }
+
+        document.getElementById("warning").textContent = "";
+
+        const mean = c;
+        const probability = (c == x) ? 1 : 0;
+
+        document.getElementById("degenerate-mean").textContent = mean;
+        document.getElementById("degenerate-probability").textContent = probability;
+    }
+    degenerate_c.addEventListener("input", updateDegenerate);
+    degenerate_x.addEventListener("input", updateDegenerate);
+    // #endregion
+    // #region Hypergeometric
+    function hypergeometricProbability(n, k, sample_n) {
+        return function(x) {
+            return Number((combination(k, x) * combination((n-k), (sample_n-x))))/Number(combination(n, sample_n));
+        }
+    }
+
+    const hypergeometric_n = document.getElementById("hypergeometric-n");
+    const hypergeometric_k = document.getElementById("hypergeometric-k");
+    const hypergeometric_sample_n = document.getElementById("hypergeometric-sample-n");
+    const hypergeometric_x = document.getElementById("hypergeometric-x");
+
+    function updateHypergeometric() {
+        const n = Number(hypergeometric_n.value);
+        const k = Number(hypergeometric_k.value);
+        const sample_n = Number(hypergeometric_sample_n.value);
+        const x = Number(hypergeometric_x.value);
+
+        if (n <= 1) {
+            document.getElementById("warning").textContent = "N must be larger than 1";
+            document.getElementById("hypergeometric-mean").textContent = 0;
+            document.getElementById("hypergeometric-variance").textContent = 0;
+            document.getElementById("hypergeometric-sd").textContent = 0;
+            return;
+        }
+
+        if (0 > k || k > n) {
+            document.getElementById("warning").textContent = "K must be between 0 and N";
+            document.getElementById("hypergeometric-mean").textContent = 0;
+            document.getElementById("hypergeometric-variance").textContent = 0;
+            document.getElementById("hypergeometric-sd").textContent = 0;
+            return;
+        }
+
+        if (0 > sample_n || sample_n > n) {
+            document.getElementById("warning").textContent = "n must be between 0 and N";
+            document.getElementById("hypergeometric-mean").textContent = 0;
+            document.getElementById("hypergeometric-variance").textContent = 0;
+            document.getElementById("hypergeometric-sd").textContent = 0;
+            return;
+        }
+
+        if (x > k || x < 0 || x > sample_n) {
+            document.getElementById("warning").textContent = "x must be between 0 and n, and be smaller than K";
+            document.getElementById("hypergeometric-probability").textContent = 0;
+            document.getElementById("hypergeometric-cumulative").textContent = 0;
+            return;
+        }
+
+        if ((sample_n - x) > (n-k)) {
+            document.getElementById("warning").textContent = "n-x must be N-K or smaller";
+            document.getElementById("hypergeometric-probability").textContent = 0;
+            document.getElementById("hypergeometric-cumulative").textContent = 0;
+            return;
+        }
+
+        document.getElementById("warning").textContent = "";
+
+        const mean = sample_n*(k/n);
+        const variance = (sample_n*(k/n))*((n-k)/n)*((n-sample_n)/(n-1));
+        const sd = Math.sqrt(variance);
+        const aux = hypergeometricProbability(n,k,sample_n);
+        const probability = aux(x);
+        const cumulative = discreteCumulative(aux, x);
+
+        document.getElementById("hypergeometric-mean").textContent = mean.toFixed(4);
+        document.getElementById("hypergeometric-variance").textContent = variance.toFixed(4);
+        document.getElementById("hypergeometric-sd").textContent = sd.toFixed(4);
+        document.getElementById("hypergeometric-probability").textContent = probability.toFixed(5);
+        document.getElementById("hypergeometric-cumulative").textContent = cumulative.toFixed(5);
+    }
+
+    hypergeometric_n.addEventListener("input", updateHypergeometric);
+    hypergeometric_k.addEventListener("input", updateHypergeometric);
+    hypergeometric_sample_n.addEventListener("input", updateHypergeometric);
+    hypergeometric_x.addEventListener("input", updateHypergeometric);
+    // #endregion
+    // #region Binomial negative
+    function negativeBinomialProbability(r, p) {
+        return function(x) {
+            return Number((combination((x+r-1), x)))*((1-p)**x)*(p**r);
+        }
+    }
+
+    const negative_binomial_r = document.getElementById("negative-binomial-r")
+    const negative_binomial_p = document.getElementById("negative-binomial-p")
+    const negative_binomial_x = document.getElementById("negative-binomial-x")
+
+    function updateNegativeBinomial() {
+        const r = Number(negative_binomial_r.value);
+        const p = Number(negative_binomial_p.value);
+        const x = Number(negative_binomial_x.value);
+
+        if(p > 1 || p < 0) {
+            document.getElementById("warning").textContent = "p must be between 0 and 1";
+            document.getElementById("negative-binomial-mean").textContent = 0;
+            document.getElementById("negative-binomial-variance").textContent = 0;
+            document.getElementById("negative-binomial-sd").textContent = 0;
+            document.getElementById("negative-binomial-probability").textContent = 0;
+            document.getElementById("negative-binomial-cumulative").textContent = 0;
+            return;
+        }
+
+        if(r <= 0) {
+            document.getElementById("warning").textContent = "r must be greater than 0";
+            document.getElementById("negative-binomial-mean").textContent = 0;
+            document.getElementById("negative-binomial-variance").textContent = 0;
+            document.getElementById("negative-binomial-sd").textContent = 0;
+            document.getElementById("negative-binomial-probability").textContent = 0;
+            document.getElementById("negative-binomial-cumulative").textContent = 0;
+            return;
+        }
+
+        if(x < 0) {
+            document.getElementById("warning").textContent = "x must be 0 or greater";
+            document.getElementById("negative-binomial-mean").textContent = 0;
+            document.getElementById("negative-binomial-variance").textContent = 0;
+            document.getElementById("negative-binomial-sd").textContent = 0;
+            document.getElementById("negative-binomial-probability").textContent = 0;
+            document.getElementById("negative-binomial-cumulative").textContent = 0;
+            return;
+        }
+
+        document.getElementById("warning").textContent = "";
+
+        const mean = r*((1-p)/p);
+        const variance = r*((1-p)/p**2);
+        const sd = Math.sqrt(variance);
+        const aux = negativeBinomialProbability(r, p);
+        const probability = aux(x);
+        const cumulative = discreteCumulative(aux, x);
+
+        document.getElementById("negative-binomial-mean").textContent = mean.toFixed(4);
+        document.getElementById("negative-binomial-variance").textContent = variance.toFixed(4);
+        document.getElementById("negative-binomial-sd").textContent = sd.toFixed(4);
+        document.getElementById("negative-binomial-probability").textContent = probability.toFixed(5);
+        document.getElementById("negative-binomial-cumulative").textContent = cumulative.toFixed(5);
+    }
+
+    negative_binomial_r.addEventListener("input", updateNegativeBinomial);
+    negative_binomial_p.addEventListener("input", updateNegativeBinomial);
+    negative_binomial_x.addEventListener("input", updateNegativeBinomial);
     // #endregion
     // #endregion 
 
